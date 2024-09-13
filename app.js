590a98ddfd0dc1124669df5b9b7830f2f5a80799
Marjeremy <gaoat856@gmail.com>
2024-09-13 21:05:09 +0800

    js-practice- commit

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Hello World!");
+let myname = "Mark Jeremy Gaoat";
+console.log = ("Myname is ", myname);
+
+console.log = ("USING VARIABLE ");
+let name = "Mark Jeremy";
+let age = "19";
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+console.log("USING NUMBERS");
+let num1 = 30;
+let num2 = 10;
+let sum = num1 + num2;
+
+console.log(sum);
+
+console.log("USING BOOLEANS");
+let programming = true;
+console.log("I love programming ?", programming);
+
+console.log("USING COMPARISON OPERATOR");
+let a = 10;
+let b = 15;
+
+if (a == b){
+    console.log(a, 'and' ,b, 'is equal');
+    } else{
+        console.log(a, 'and' ,b, 'is not equal');
+        }
